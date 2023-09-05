efbff37263bce2fd583047d8127932c484c44222
Felix Maier <fmai@allix.ch>
2023-09-05 16:20:35 +0200

    NewtonsoftJson
    no worky

--- Test1/Server/Program.cs
+++ Test1/Server/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddAuthentication()
 
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(op => op.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.ignore);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
