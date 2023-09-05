a8070ca2a1fce956cf3033ecb6e37e88a653c37d
Felix Maier <fmai@allix.ch>
2023-09-05 11:47:01 +0200

    Changed Endpoint Static for better readability

--- Test1/Client/Static/Endpoints.cs
+++ Test1/Client/Static/Endpoints.cs
@@ -4,11 +4,11 @@
     {
         public static string Prefix = "api";
 
-        public static string BookingsEndpoint = $"{Prefix}/bookings/";
-        public static string ColorsEndpoint = $"{Prefix}/colors/";
-        public static string CustomersEndpoint = $"{Prefix}/customers/";
-        public static string MakesEndpoint = $"{Prefix}/makes/";
-        public static string ModelsEndpoint = $"{Prefix}/models/";
-        public static string VehiclesEndpoint = $"{Prefix}/vehicles/";
+        public static string BookingsEndpoint = $"{Prefix}/bookings";
+        public static string ColorsEndpoint = $"{Prefix}/colors";
+        public static string CustomersEndpoint = $"{Prefix}/customers";
+        public static string MakesEndpoint = $"{Prefix}/makes";
+        public static string ModelsEndpoint = $"{Prefix}/models";
+        public static string VehiclesEndpoint = $"{Prefix}/vehicles";
     }
 }
